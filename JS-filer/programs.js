07af7687f82a4f0010c25819b2a8fe6d9b1db65d
Hanna Bina Nilsen <bina.99@live.se>
2022-05-09 16:05:52 +0200

    lagt in tre drop-down filtreringar

---
+++ JS-filer/programs.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// function renderFieldOption() {
+//     let field = document.getElementById("field");
+//     let arr = [];
+//     for (let i = 0; i < FIELDS.length; i++) {
+//         let option = document.createElement("option");
+//         option.innerHTML = `<option value="${FIELDS.name}">${FIELDS.name}</option>`
+//         field.appendChild("option")
+//         arr.push(option)
+//     }
+//     return arr.toString().split(",").join("");
+// }
+
+// renderFieldOption()
+
+function renderPrograms()
