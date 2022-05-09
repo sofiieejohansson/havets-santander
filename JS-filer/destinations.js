214f68c0a3705fb1833a02fcd15877233c1c5dac
MadeByGabbe <97948127+MadeByGabbe@users.noreply.github.com>
2022-05-09 13:29:19 +0200

    renderCities är klar
    renderCities funkar men den försöker lägga till bilderna två gånger så det bildas 34 errors

--- JS-filer/destinations.js
+++ JS-filer/destinations.js
@@ -1,13 +1,31 @@
 function renderCountries () {
     let countryName = COUNTRIES.filter((country) => country.name);
-    let cityContainer = document.getElementById('destinations-container')
+    let cityContainer = document.getElementById('destinations-container');
     for (let i = 0; i < countryName.length; i++) {
         cityContainer.innerHTML += `
         <div class="country-container">
             <h1>${countryName[i].name}</h1>
-            ${renderCities()}
-        </div>`
+            <div class="cities-container">
+                <div class="city-box"></div>
+            </div>
+        </div>`;
+        renderCities(i)
     }
     return countryName
 }
 
+function renderCities (counter) {
+    for(let city of CITIES) {
+
+        if(COUNTRIES[counter].id == city.countryID) {
+
+            document.querySelector(`#destinations-container > div:last-child > div > .city-box`).innerHTML += `
+            <img class="city-picture" src="Images/${city.imagesNormal[0]} alt="">
+            <h2 class="city-h2">${city.name}</div> `
+
+
+            }
+        }
+}
+
+renderCountries()
