82adfdf4ed77d96a1262f604da7bd889b89e2a84
MadeByGabbe <97948127+MadeByGabbe@users.noreply.github.com>
2022-05-09 12:48:20 +0200

    renderCountries function grund är lagd

--- JS-filer/destinations.js
+++ JS-filer/destinations.js
@@ -1,3 +1,13 @@
-function renderCities (database) {
-    
+function renderCountries () {
+    let countryName = COUNTRIES.filter((country) => country.name);
+    let cityContainer = document.getElementById('destinations-container')
+    for (let i = 0; i < countryName.length; i++) {
+        cityContainer.innerHTML += `
+        <div class="country-container">
+            <h1>${countryName[i].name}</h1>
+            ${renderCities()}
+        </div>`
+    }
+    return countryName
 }
+
