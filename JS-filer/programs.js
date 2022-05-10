22e975f0c65c3c061b314fcb068f14dfe911646b
Hanna Bina Nilsen <bina.99@live.se>
2022-05-10 16:37:55 +0200

    Gjort filtrering på field

--- JS-filer/programs.js
+++ JS-filer/programs.js
@@ -1,17 +1,99 @@
 "use strict";
 
-// function renderFieldOption() {
-//     let field = document.getElementById("field");
-//     let arr = [];
-//     for (let i = 0; i < FIELDS.length; i++) {
-//         let option = document.createElement("option");
-//         option.innerHTML = `<option value="${FIELDS.name}">${FIELDS.name}</option>`
-//         field.appendChild("option")
-//         arr.push(option)
-//     }
-//     return arr.toString().split(",").join("");
-// }
+let filteredProgrammes = [];
 
-// renderFieldOption()
 
-function renderPrograms()
+let programmes = DB.PROGRAMMES
+
+function showProgram(id) {
+    let div = document.createElement("div");
+    let program = DB.PROGRAMMES[id]; 
+    div.classList = "programme-box";
+    div.innerHTML = `
+    <header>${program.name} - CITY, COUNTRY</header>
+    <div id="programme-info">
+        <p>Local Students: ${program.localStudents}</p>
+        <p>Exchange Students: ${program.exchangeStudents}</p>
+        <p>${showField(program)}</p>
+        <p>kalla på showLanguage-funktionen</p>
+    </div>`
+
+    return div;
+}
+
+function showProgrammes(programmes) {
+    let programmesElement = document.getElementById("programme-container");
+    for (let programme of programmes){
+        let programmeElement = showProgram(programme.id);
+        programmesElement.appendChild(programmeElement);
+    }
+}
+
+function showField(programme) {
+    let fieldInfo = DB.FIELDS;
+
+    return fieldInfo.find(field => { 
+        if (programme.subjectID == field.id){
+            return field.name;
+        }
+    });
+}
+
+function showLanguage(programme) {
+
+
+    return "languageInfo"
+}
+
+const feildSelect = document.getElementById("field");
+feildSelect.addEventListener("change", function(){
+    filterFeild(feildSelect.value);
+    showProgrammes(filteredProgrammes)
+});
+
+
+
+function filterFeild (fieldOption) {
+    if (isFilteredEmpty()) {
+        filteredProgrammes = filteredProgrammes.filter(programme => {
+            if (fieldOption == programme.subjectID) {
+                return programme;
+            } 
+        });
+    } else {
+        filteredProgrammes = DB.PROGRAMMES.filter(programme => {
+            if (fieldOption == programme.subjectID) {
+                return programme;
+            } 
+        });
+    }
+}
+
+function isFilteredEmpty () {
+    return filteredProgrammes.length
+}
+
+function filterLanguage () {
+
+}
+
+function filterLevel () {
+
+}
+
+function filterCountry () {
+    //if (country is filled) {
+    //    enable filterCity
+    //}
+}
+
+function filterCity () {
+    // endast baserat på valt land
+}
+
+function onChange () {
+    // eventlistener för klick och välj på dropdown
+    // uppdaterar listan allt eftersom man väljer olika filtreringar
+}
+
+showProgrammes(filteredProgrammes);
