a4c3a2fe7bf2b8c11b53986dd660f466cd316aa2
MadeByGabbe <97948127+MadeByGabbe@users.noreply.github.com>
2022-05-12 13:36:52 +0200

    Länder är med efter sökning

--- JS-filer/destinations.js
+++ JS-filer/destinations.js
@@ -57,47 +57,10 @@ function renderCities (counter, array) {
         }
 }
 
-renderCountries()
-
-
-// let input = document.getElementById("search-input");
-
-
-// function findCity() {
-
-//     let destination = document.getElementById("destinations-container");
-//     destination.innerHTML = ""
-//     for (city of CITIES){
-//         let city = CITIES.filter((city) =>
-//         city.name.toLowerCase().startsWith(input.value.toLowerCase()));
-
-//         for (let country of COUNTRIES){
-//             // if (city.id == country.id) {
-//                 destination.innerHTML += `
-//                 <div class="country-container">
-//                     <h1 class= "country-h1">${country.name}</h1>
-//                     <div class="cities-container">
-//                         <div class="city-box">
-//                         <img class="city-picture" src="Images/${city[0].imagesNormal[0]}" alt="">
-//                         <h2 class="city-h2">${city.name}</h2>
-//                         </div> 
-        
-//                     </div>
-//                 </div>`
-//             // }
-
-
-
-//         }
-//     return console.log(city); 
-    
-//   }}
 
 let elementSelector = (select) => {
     return document.querySelector(select);
 }
-
-elementSelector("#search-input").addEventListener("keyup", getResults);
 
 
 function getResults (){
@@ -110,37 +73,35 @@ function getResults (){
     if (input.length > 0) {
 
         for(let i = 0; i < CITIES.length; i++) {
-            if(CITIES[i].name.toLocaleLowerCase().startsWith(input)) {
-                document.querySelector(`#destinations-container`).innerHTML += `
+            if (CITIES[i].name.toLocaleLowerCase().startsWith(input)) {
                 
-                <div class="country-container">
-                    <h1 class="country-h1"> </h1>
-                    <div class="cities-container">
-                        <div class="city-box">
-                            <img class="city-picture" src="Images/${CITIES[i].imagesNormal[0]}" alt="">
-                            <h2 class="city-h2">${CITIES[i].name}</h2>
-                        </div> 
-                    </div>
-                </div>`
+                for (let country of COUNTRIES) {
+                    if (country.id == CITIES[i].countryID) {
+                        document.querySelector(`#destinations-container`).innerHTML += `
+                        
+                        
+                        <div class="country-container">
+                            <h1 class="country-h1">${country.name}</h1>
+                            <div class="cities-container">
+                                <div class="city-box">
+                                    <img class="city-picture" src="Images/${CITIES[i].imagesNormal[0]}" alt="">
+                                    <h2 class="city-h2">${CITIES[i].name}</h2>
+                                </div> 
+                            </div>
+                        </div>`
 
+                    }
+                }
             }
-            
-        }
-        
+        } 
     }
     else {
         renderCountries();
     }
 }
 
-
-
-function test(){
-   
-
-
-}
-
+elementSelector("#search-input").addEventListener("keyup", getResults);
+renderCountries()
 
 
   
