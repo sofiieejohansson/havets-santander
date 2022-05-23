f69de750fbcad3ada25a2b4563c138cc54911e3a
Max Erixon <maxerixon112@gmail.com>
2022-05-23 16:52:48 +0200

    format test

--- JS-filer/destinations.js
+++ JS-filer/destinations.js
@@ -2,88 +2,87 @@
 
 let index = 0;
 
-localStorage.clear()
+localStorage.clear();
 
-function renderCountries () {
-    let countryName = COUNTRIES.filter((country) => country)
-    .sort((a, b) => {
-        if(a.name > b.name) {
-            return 1;
-        }
-        else if (a.name < b.name) {
-            return -1;
-        }
-        return 0;
-    });
+function renderCountries() {
+  let countryName = COUNTRIES.filter((country) => country).sort((a, b) => {
+    if (a.name > b.name) {
+      return 1;
+    } else if (a.name < b.name) {
+      return -1;
+    }
+    return 0;
+  });
 
-    let countryContainer = document.getElementById('destinations-container');
-    for (let i = 0; i < countryName.length; i++) {
-        countryContainer.innerHTML += `
+  let countryContainer = document.getElementById("destinations-container");
+  for (let i = 0; i < countryName.length; i++) {
+    countryContainer.innerHTML += `
         <div class="country-container">
             <h1 class= "country-h1">${countryName[i].name}</h1>
             <div class="cities-container" id="${countryName[i].name}"></div>
         </div>`;
-        renderCities(i, countryName)
-    }
-    setEventHandler()
-    squareAdHandler()
+    renderCities(i, countryName);
+  }
+  setEventHandler();
+  squareAdHandler();
 }
 
-function renderCities (counter, array) {
-    let cities = CITIES.filter((city) => city).sort((a, b) => {
-        if(a.name > b.name) {
-            return 1;
-        }
-        else if (a.name < b.name) {
-            return -1;
-        }
-        return 0;
-    });
+function renderCities(counter, array) {
+  let cities = CITIES.filter((city) => city).sort((a, b) => {
+    if (a.name > b.name) {
+      return 1;
+    } else if (a.name < b.name) {
+      return -1;
+    }
+    return 0;
+  });
 
-    for(let city of cities) {
-        if(array[counter].id == city.countryID) {
-            document.querySelector(`#destinations-container > div:last-child > div`).innerHTML += `
+  for (let city of cities) {
+    if (array[counter].id == city.countryID) {
+      document.querySelector(
+        `#destinations-container > div:last-child > div`
+      ).innerHTML += `
             <div class="city-box" id="${city.name}">
                 <img class="city-picture" src="Images/${city.imagesNormal[0]}" alt="">
                 <h2 class="city-h2">${city.name}</h2>
-            </div> `
-        }
-    }    
+            </div> `;
+    }
+  }
 }
 
 function squareAdHandler() {
-    let squareAd1 = document.createElement("div")
-    let squareAd2 = document.createElement("div")
-    let squareAd3 = document.createElement("div")
-    squareAd1.className = "square-Ad"
-    squareAd2.className = "square-Ad"
-    squareAd3.className = "square-Ad"
-    document.querySelector("#Chile").appendChild(squareAd1)
-    document.querySelector("#Spain").appendChild(squareAd2)
-    document.getElementById("United Kingdom").appendChild(squareAd3)
+  let squareAd1 = document.createElement("div");
+  let squareAd2 = document.createElement("div");
+  let squareAd3 = document.createElement("div");
+  squareAd1.className = "square-Ad";
+  squareAd2.className = "square-Ad";
+  squareAd3.className = "square-Ad";
+  document.querySelector("#Chile").appendChild(squareAd1);
+  document.querySelector("#Spain").appendChild(squareAd2);
+  document.getElementById("United Kingdom").appendChild(squareAd3);
 }
 
 function setEventHandler() {
-    for(let city of DB.CITIES) {
-        let cityBox = document.getElementById(`${city.name}`);
-        if (cityBox != null) {
-            cityBox.addEventListener("click", function(){renderCitiesPopup(city.name)})
-        }
+  for (let city of DB.CITIES) {
+    let cityBox = document.getElementById(`${city.name}`);
+    if (cityBox != null) {
+      cityBox.addEventListener("click", function () {
+        renderCitiesPopup(city.name);
+      });
     }
+  }
 }
 
-function getCitiesAfterSearch (){
-    let input = document.querySelector("#search-input").value;
-    document.querySelector("#destinations-container").innerHTML = ""
+function getCitiesAfterSearch() {
+  let input = document.querySelector("#search-input").value;
+  document.querySelector("#destinations-container").innerHTML = "";
 
-    if (input.length > 0) {
-        for (let i = 0; i < CITIES.length; i++) {
-            
-            if (CITIES[i].name.toLocaleLowerCase().startsWith(input)) {
-                for (let country of COUNTRIES) {
-
-                    if (country.id == CITIES[i].countryID) {
-                        document.querySelector(`#destinations-container`).innerHTML += `
+  if (input.length > 0) {
+    for (let i = 0; i < CITIES.length; i++) {
+      if (CITIES[i].name.toLocaleLowerCase().startsWith(input)) {
+        for (let country of COUNTRIES) {
+          if (country.id == CITIES[i].countryID) {
+            document.querySelector(`#destinations-container`).innerHTML += `
                         <div class="country-container">
                             <h1 class="country-h1">${country.name}</h1>
                             <div class="cities-container">
@@ -93,24 +92,22 @@ function getCitiesAfterSearch (){
                                 </div> 
                             </div>
                         </div>`;
-                    }
-                }
-            }
-        } 
-    setEventHandler()
+          }
+        }
+      }
     }
-    else {
-        renderCountries();
-    }
+    setEventHandler();
+  } else {
+    renderCountries();
+  }
 }
 
-function renderCitiesPopup (cityName) {
-    for(let i= 0; i < CITIES.length; i++ ) {
-       let city = CITIES[i]
-       
-        if (city.name == cityName) {
-            
-           document.querySelector(`#destinations-container`).innerHTML += `
+function renderCitiesPopup(cityName) {
+  for (let i = 0; i < CITIES.length; i++) {
+    let city = CITIES[i];
+
+    if (city.name == cityName) {
+      document.querySelector(`#destinations-container`).innerHTML += `
            <div class="destination-popup">
                 <div class="close"></div>
                 <div class="city-picture-popup" style="background-image:url(Images/${city.imagesBig[0]});">
@@ -135,50 +132,50 @@ function renderCitiesPopup (cityName) {
                     </div>
                 </div>
             </div>
-            <div class="background-white"></div>`
-         popupCommentHandler(city.id)
-         commentButtonHandler(city.id)
-         sendToPrograms(city.id, city.countryID)
-        }
+            <div class="background-white"></div>`;
+      popupCommentHandler(city.id);
+      commentButtonHandler(city.id);
+      sendToPrograms(city.id, city.countryID);
     }
-    closeButton(); 
-}  
+  }
+  closeButton();
+}
 
-function sendToPrograms(cityID, cityCountry){
-    document.querySelector(".programs-in-city").onclick = function() {
-        sessionStorage.setItem("cityID", cityID);
-        sessionStorage.setItem("cityCountry", cityCountry)
-        location.href = "programs.html"
-    }
+function sendToPrograms(cityID, cityCountry) {
+  document.querySelector(".programs-in-city").onclick = function () {
+    sessionStorage.setItem("cityID", cityID);
+    sessionStorage.setItem("cityCountry", cityCountry);
+    location.href = "programs.html";
+  };
 }
 
 function closeButton() {
-    document.querySelector(".close").addEventListener("click", function () {
-        document.querySelector(".destination-popup").remove();
-        setEventHandler()
-        document.querySelector(".background-white").remove();
-    })
+  document.querySelector(".close").addEventListener("click", function () {
+    document.querySelector(".destination-popup").remove();
+    setEventHandler();
+    document.querySelector(".background-white").remove();
+  });
 }
 
-function popupCommentHandler(cityID){
-    let cityComments = COMMENTS_CITY.filter((comment) => {
-        return comment.cityID == cityID
-    })
-    if(index < 0){
-        index = cityComments.length - 1
-    }
-    if(index > cityComments.length - 1){
-        index = 0
-    }
+function popupCommentHandler(cityID) {
+  let cityComments = COMMENTS_CITY.filter((comment) => {
+    return comment.cityID == cityID;
+  });
+  if (index < 0) {
+    index = cityComments.length - 1;
+  }
+  if (index > cityComments.length - 1) {
+    index = 0;
+  }
 
+  let comments = cityComments[index];
+
+  if (cityComments.length > 0) {
+    if (index == cityComments.length) {
+      index = 0;
+    }
     let comments = cityComments[index];
-    
-    if(cityComments.length > 0){
-        if(index == cityComments.length){
-            index = 0
-        }
-        let comments = cityComments[index]
-        document.querySelector(".popup-comments").innerHTML = `
+    document.querySelector(".popup-comments").innerHTML = `
         <h3 class="comment-alias">${comments.alias}, ${comments.date.year}-${comments.date.month}-${comments.date.day}</h3>
         <h3 class="comment-text">${comments.text}</h3>
         <div class="ratings">
@@ -186,113 +183,119 @@ function popupCommentHandler(cityID){
             <h4 class="comment-ratings">Food: ${comments.stars.food}/5 <img class="star-icon" src="Images/star-icon.png" alt="" /></h4>
             <h4 class="comment-ratings">Acomodation: ${comments.stars.accomodation}/5 <img class="star-icon" src="Images/star-icon.png" alt="" /></h4>
         </div>    
-        `
-    }
-    else {
-        document.querySelector(".popup-comments").innerHTML = `
+        `;
+  } else {
+    document.querySelector(".popup-comments").innerHTML = `
         <h3 class="no-comments">No comments found</h3>
         <div class="gubbe"></div>
-        `
-    }
+        `;
+  }
 }
 
 function commentButtonHandler(cityID) {
-    document.querySelector(".left-button").addEventListener("click", function() {
-        index--; 
-        popupCommentHandler(cityID)})
-    document.querySelector(".right-button").addEventListener("click", function() {
-        index++; 
-        popupCommentHandler(cityID);})
+  document.querySelector(".left-button").addEventListener("click", function () {
+    index--;
+    popupCommentHandler(cityID);
+  });
+  document
+    .querySelector(".right-button")
+    .addEventListener("click", function () {
+      index++;
+      popupCommentHandler(cityID);
+    });
 }
 
+let ukPin = document.querySelector(".uk-pin");
+ukPin.style.cursor = "pointer";
 
-let ukPin = document.querySelector(".uk-pin")
-ukPin.style.cursor="pointer"
+ukPin.addEventListener("click", function () {
+  ukPin.style.cursor = "pointer";
+  document
+    .querySelector(`#destinations-container > div:last-child`)
+    .scrollIntoView();
+});
 
-ukPin.addEventListener("click", function(){
-    ukPin.style.cursor ="pointer"
-    document.querySelector(`#destinations-container > div:last-child`).scrollIntoView();
-})
+let usaPin = document.querySelector(".usa-pin");
+usaPin.style.cursor = "pointer";
 
+usaPin.addEventListener("click", function () {
+  usaPin.style.cursor = "pointer";
+  document
+    .querySelector(`#destinations-container > div:nth-child(8)`)
+    .scrollIntoView();
+});
 
-let usaPin = document.querySelector(".usa-pin")
-usaPin.style.cursor="pointer"
+let swePin = document.querySelector(".sweden-pin");
+swePin.style.cursor = "pointer";
 
-usaPin.addEventListener("click", function(){
-    usaPin.style.cursor ="pointer"
-    document.querySelector(`#destinations-container > div:nth-child(8)`).scrollIntoView();
-})
+swePin.addEventListener("click", function () {
+  swePin.style.cursor = "pointer";
+  document
+    .querySelector(`#destinations-container > div:nth-child(7)`)
+    .scrollIntoView();
+});
 
+let spaPin = document.querySelector(".spain-pin");
+spaPin.style.cursor = "pointer";
 
-let swePin = document.querySelector(".sweden-pin")
-swePin.style.cursor="pointer"
+spaPin.addEventListener("click", function () {
+  spaPin.style.cursor = "pointer";
+  document
+    .querySelector(`#destinations-container > div:nth-child(6)`)
+    .scrollIntoView();
+});
 
-swePin.addEventListener("click", function(){
-    swePin.style.cursor ="pointer"
-    document.querySelector(`#destinations-container > div:nth-child(7)`).scrollIntoView();
-})
+let mexPin = document.querySelector(".mexico-pin");
+mexPin.style.cursor = "pointer";
 
+mexPin.addEventListener("click", function () {
+  mexPin.style.cursor = "pointer";
+  document
+    .querySelector(`#destinations-container > div:nth-child(5)`)
+    .scrollIntoView();
+});
 
-let spaPin = document.querySelector(".spain-pin")
-spaPin.style.cursor="pointer"
+let fraPin = document.querySelector(".france-pin");
+fraPin.style.cursor = "pointer";
 
-spaPin.addEventListener("click", function(){
-    spaPin.style.cursor ="pointer"
-    document.querySelector(`#destinations-container > div:nth-child(6)`).scrollIntoView();
-})
+fraPin.addEventListener("click", function () {
+  fraPin.style.cursor = "pointer";
+  document
+    .querySelector(`#destinations-container > div:nth-child(4)`)
+    .scrollIntoView();
+});
 
+let chiPin = document.querySelector(".chile-pin");
+chiPin.style.cursor = "pointer";
 
-let mexPin = document.querySelector(".mexico-pin")
-mexPin.style.cursor="pointer"
+chiPin.addEventListener("click", function () {
+  chiPin.style.cursor = "pointer";
+  document
+    .querySelector(`#destinations-container > div:nth-child(3)`)
+    .scrollIntoView();
+});
 
-mexPin.addEventListener("click", function(){
-    mexPin.style.cursor ="pointer"
-    document.querySelector(`#destinations-container > div:nth-child(5)`).scrollIntoView();
-})
+let ausPin = document.querySelector(".australia-pin");
+ausPin.style.cursor = "pointer";
 
+ausPin.addEventListener("click", function () {
+  ausPin.style.cursor = "pointer";
+  document
+    .querySelector(`#destinations-container > div:nth-child(2)`)
+    .scrollIntoView();
+});
 
-let fraPin = document.querySelector(".france-pin")
-fraPin.style.cursor="pointer"
+let argPin = document.querySelector(".argentina-pin");
+argPin.style.cursor = "pointer";
 
-fraPin.addEventListener("click", function(){
-    fraPin.style.cursor ="pointer"
-    document.querySelector(`#destinations-container > div:nth-child(4)`).scrollIntoView();
-})
+argPin.addEventListener("click", function () {
+  argPin.style.cursor = "pointer";
+  document
+    .querySelector(`#destinations-container > div:nth-child(1)`)
+    .scrollIntoView();
+});
 
-
-let chiPin = document.querySelector(".chile-pin")
-chiPin.style.cursor="pointer"
-
-chiPin.addEventListener("click", function(){
-    chiPin.style.cursor ="pointer"
-    document.querySelector(`#destinations-container > div:nth-child(3)`).scrollIntoView();
-})
-
-
-let ausPin = document.querySelector(".australia-pin")
-ausPin.style.cursor="pointer"
-
-ausPin.addEventListener("click", function(){
-    ausPin.style.cursor ="pointer"
-    document.querySelector(`#destinations-container > div:nth-child(2)`).scrollIntoView();
-})
-
-
-let argPin = document.querySelector(".argentina-pin")
-argPin.style.cursor="pointer"
-
-argPin.addEventListener("click", function(){
-    argPin.style.cursor ="pointer"
-    document.querySelector(`#destinations-container > div:nth-child(1)`).scrollIntoView();
-})
-   
-document.querySelector("#search-input").addEventListener("keyup", getCitiesAfterSearch);
-renderCountries()
-
-
-
-
-
-
-
-  
+document
+  .querySelector("#search-input")
+  .addEventListener("keyup", getCitiesAfterSearch);
+renderCountries();
